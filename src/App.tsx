9738f739663b9e080607b3e40852649e1770cf79
Isaac Gemal <isaacgemal@gmail.com>
2024-09-23 13:11:46 -0400

    Add flickering grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
 import './App.css'
 import BlurIn from "@/components/magicui/blur-in";
+import FlickeringGrid from "@/components/magicui/flickering-grid";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Welcome</h1>
+    <div className='Background'>
+    <FlickeringGrid
+        className="z-0 absolute inset-0 size-full"
+        squareSize={40}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.1}
+        flickerChance={0.1}
+      /> 
       <h1>
-      <BlurIn word={"Isaac Gemal"}></BlurIn>
+        <BlurIn word={"Isaac Gemal"}></BlurIn>
       </h1>
       <h4>
         <BlurIn duration={1.5} word={"Builds stuff"}></BlurIn>
       </h4>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       <h2>Welcome to my website!</h2> 
+      <div>
+        <h2>Welcome to my website!</h2>
       </div>
+    </div>
     </>
   )
 }
